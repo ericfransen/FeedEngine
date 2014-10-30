a754b89fbb401db845f2fe22703d12eca4643a6a
ericfransen <fransenep@gmail.com>
2014-10-30 14:53:17 -0600

    adds twitter profile pic to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141030155241) do
+ActiveRecord::Schema.define(version: 20141030204339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20141030155241) do
     t.string "uid"
     t.string "provider"
     t.string "phone_number"
+    t.string "twitter_pic"
   end
 
 end
