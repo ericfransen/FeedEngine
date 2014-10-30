e9fec1ff900d8951c20e069799c09957c09d6633
Tan Doan <tdoan87@gmail.com>
2014-10-30 10:37:04 -0600

    installed the figaro gem, implemented twitter authoriztion

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   root 'welcome#index'
   resources :goals, only: [:index]
   resources :github_goals
+
+  get '/login' => redirect('/auth/twitter'), as: :login
+  get '/logout' => 'sessions#destroy', as: :logout
+  get '/auth/twitter/callback', to: 'sessions#create'
+  get '/auth/failure', to: 'sessions#failure'
+
 end
