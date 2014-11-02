06f16199676cfca7e08be0fc394a629a95d3b468
tdoan87 <tdoan87@gmail.com>
2014-11-01 18:14:22 -0600

    Setup webmock to stub out request to github api, also added a test to test the first methods get_the_json and push_events. All test passing.

--- spec/models/github_goal_spec.rb
+++ spec/models/github_goal_spec.rb
@@ -1,3 +1,16 @@
+require 'rails_helper'
+
 describe GithubGoal, type: :model do
   it {should belong_to :user }
+
+  
+
+  let(:github_goal) { GithubGoal.create(username: 'dglunz')}
+
+  it 'can retrieve push events' do
+    push_events = github_goal.push_events
+    push_events.each do |event|
+      expect(event['type']).to eq('PushEvent')
+    end
+  end
 end
