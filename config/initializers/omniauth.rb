64d09b7c8f30b67bf2839002d47d1f2171cffec4
Andrew Watkins <andrew.watkinz@gmail.com>
2014-11-03 17:07:02 -0700

    shhhhh secret hardcoded

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['twitter_key'], ENV['twitter_password']
+  provider :twitter, 'jpachkf9nkUmZJD93pzAWqIOn', '6ARBasFb020lxJGlDCijtoG1hw2XBHUBEgCsY8h2kJi65OXlqx'
 end
