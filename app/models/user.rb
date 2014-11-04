1192f5a40eab0a173c016935170afefddf6a10fa
Andrew Watkins <andrew.watkinz@gmail.com>
2014-11-04 13:46:08 -0500

    changes assets to work on vps

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
-  validates :name, :twitter_pic, presence: true
+  validates :name, presence: true
   has_many :github_goals
 
   def self.find_or_create_by_auth(auth_data)
+ 
     user = self.find_or_initialize_by(provider: auth_data["provider"], uid: auth_data['uid'])
     unless user.persisted? && user.name == auth_data['info']['uid']
       user.name        = auth_data['info']['name']
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'jpachkf9nkUmZJD93pzAWqIOn', '6ARBasFb020lxJGlDCijtoG1hw2XBHUBEgCsY8h2kJi65OXlqx'
+    provider :twitter, 'e29FqsMgJxZiQMX2vy7VEvnza', 'Whd5Elhu9TLPjhFPKpkYEO9z1a4hghnj9wHWAcfQcjrxsqbnOp'
 end
