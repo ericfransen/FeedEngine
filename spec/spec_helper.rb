372c5824140e0fd8807acca20de686d5b1f5d608
tdoan87 <tdoan87@gmail.com>
2014-11-10 14:38:20 -0700

    changed out stubbed out request

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -31,7 +31,7 @@ body = [{
 
 RSpec.configure do |config|
   config.before(:each) do
-    stub_request(:get, "https://api.github.com/users//events")
+    stub_request(:get, "https://api.github.com/users//events?client_id=16d9d7f931c5eb4f19c3&client_secret=f78406d4b6d5a4e3abf9921e0aad80a4faaf8d5d")
                                                               .with(:headers => {'Accept'=>'*/*',
                                                                 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
                                                                 'User-Agent'=>'Faraday v0.9.0'})
