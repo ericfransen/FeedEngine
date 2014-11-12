49d70639d38e78d726235d330d9bb4dd3f814046
ericfransen <fransenep@gmail.com>
2014-11-12 13:37:04 -0700

    adds ability to grab users current goodreads books. hate you XML

--- app/models/goodreads_api.rb
+++ app/models/goodreads_api.rb
@@ -12,12 +12,21 @@ class GoodreadsApi
   #   self.client.get_request_token.authorize_url
   # end
 
-  def self.get_user_info(user)
-    response =  Faraday.get("https://www.goodreads.com/user/show/" +
+  def self.get_current_books(user)
+    url = "https://www.goodreads.com/review/list/" +
                              user.goodreads_uid +
                              ".xml?key=" +
-                             ENV['goodreads_key'].to_s)
-    parser = Nokogiri::HTML(response.body)
+                             ENV['goodreads_key'].to_s +
+                             "&v=2"
+    response =  Faraday.get(url)
 
+    parser = Nokogiri::XML(response.body)
+
+    results = parser.css('review').to_a.keep_if do |review|
+                review.css('shelf[name=currently-reading]').count > 0
+              end
+
+    current_books = results.map{|r| r.css('book title').text}
   end
+
 end
